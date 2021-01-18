3279c9705420b7aae666852c322a36151a9970dc
Miheretab Alemu <mihrtab@gmail.com>
2021-01-19 01:55:18 +0300

    fixing user page issue

---
+++ sql/change_2021_01_19.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `season` CHANGE `updated_by` `updated_by` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `user` CHANGE `updated_by` `updated_by` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `user` CHANGE `created_at` `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `user` CHANGE `updated_at` `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
