a02cd758d9a0d26a79acba59a0b0d0ad3a9de0c6
Miheretab Alemu <mihrtab@gmail.com>
2021-01-25 21:55:53 +0300

    debugging query

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -383,7 +383,7 @@ class UploadController extends AppController
                         }
                         Log::debug(serialize($thisRow));
                         $values[] = $thisRow[0];
-                        $valueParams = $thisRow[0];//"?";
+                        $valueParams = "'$thisRow[0]'";//"?";
                         $fields = $thisMetaTable[1];
                         for ($i = 1; $i < count($thisRow); $i++) {
                             if (!isset($thisMetaTable[$i + 1])) { continue; }
@@ -406,14 +406,14 @@ class UploadController extends AppController
                                 }
                                 $valueParams .= ",(SELECT id FROM {$fkTable} WHERE unique_identifier = '{$value}' ORDER BY id DESC LIMIT 1)";
                             } else {
-                                $valueParams .= utf8_encode(",$value");//",?";
+                                $valueParams .= utf8_encode(",'$value'");//",?";
                                 $values[] = utf8_encode("$value");
                             }
                             $fields .= ',' . $field;
                         }
                         // Add user ID for record
                         $fields .= ',id_user';
-                        $valueParams .= ",$userID";//",?";
+                        $valueParams .= ",'$userID'";//",?";
                         $values[] = $userID;
                         // Keep private if necessary
                         if (in_array($table, ComUpload::$canKeepPrivate) && $this->request->getData()['keep_private'] == "true") {
